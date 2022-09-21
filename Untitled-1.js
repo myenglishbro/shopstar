21e3cc109987bace495d5ff13150ce3814f78d62
Carlos Apolaya <temis_it@hotmail.com>
2022-09-21 13:01:52 -0500

    practicando

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,1 +1,17 @@
 const fecha = new Date("september 25 2022");
+
+console.log(fecha);
+let tunombre="carlos"
+let edad=25
+let pregunta=true
+let nacimient=new Date("september 25 1996")
+const libro={
+    titulo:"harry potter",
+    autor:"kjrolling",
+    fecha:"2022",
+    url:"hhttp"
+
+}
+
+const lista=new Array (tunombre,edad,pregunta,nacimient,libro);
+console.log(lista);
